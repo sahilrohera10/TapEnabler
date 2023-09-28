af5f6cfb0166702faa0542d9bdb9108d5ba2f079
Sahil <sahilrohera10@gmail.com>
2023-09-28 14:02:15 +0530

    abtdone

--- src/Pages/LandingPage.js
+++ src/Pages/LandingPage.js
@@ -1,12 +1,13 @@
 import React from "react";
 import Header2 from "../Components/Header2/Header2";
-// import Header from "../Components/Header/Header";
+import About from "../Components/About/About";
 
 export default function LandingPage() {
   return (
     <div>
-      {/* <Header /> */}
       <Header2 />
+      <br />
+      <About />
     </div>
   );
 }
